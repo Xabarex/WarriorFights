18b70d428f4eba955c49a4137d94e155648e30ed
Xabarex <jablonskibart@gmail.com>
2022-08-30 10:17:19 +0200

    added docstrings, changed variable name

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 
 
 class Weapon:
+    """hold weapon stats and in future their functions"""
     def __init__(self, name, damage):
         self.name = name
         self.damage = damage
@@ -18,6 +19,7 @@ class Weapon:
 
 
 class Champion:
+    """hold warrior stats and his functions"""
     def __init__(self, name, hit_points_maximum, weapon):
         self.name = name
         self.hit_points_maximum = hit_points_maximum
@@ -30,6 +32,7 @@ class Champion:
                f'\nmy current HP is {self.current_hit_points}'
 
     def damage_calculation(self, weapon):
+        """function used to extract hp from opponent"""
         if self.state == 1:
             self.current_hit_points = self.current_hit_points + 5 - weapon.damage
             print(f'{self.name} lost {weapon.damage - 5} hit points')
@@ -48,20 +51,20 @@ if __name__ == '__main__':
     champion_one = champion_Rogier
     champion_two = champion_Witold
     # round is used to swap current warrior
-    round = 0
+    step = 0
 
     while champion_one.current_hit_points > 0 and champion_two.current_hit_points > 0:
         print(f'Warrior {champion_one.name} have {champion_one.current_hit_points} hit points')
         print(f'Warrior {champion_two.name} have {champion_two.current_hit_points} hit points')
 
-        if round % 2 == 0:
+        if step % 2 == 0:
             current_champion = champion_one
             oponent_champion = champion_two
         else:
             current_champion = champion_two
             oponent_champion = champion_one
 
-        """check if champion is still blocking"""
+        # check if champion is still blocking
         if current_champion.state == 1:
             current_champion.state = 0
         else:
@@ -77,10 +80,10 @@ if __name__ == '__main__':
         elif action == 'p':
             pass
 
-        """checking win condition"""
+        # checking win condition
         if champion_one.current_hit_points <= 0:
             print(f'{champion_two.name} won!')
         elif champion_two.current_hit_points <= 0:
             print(f'{champion_one.name} won!')
 
-        round += 1
+        step += 1
